b17f0dfa0149f1a9b175e02bbbe1b9c9d3dc30dd
David Chen <davidc3287@gmail.com>
2025-03-31 16:01:43 -0700

    setup fixes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pool Agent
+
+## Running Script
+
+```
+python \\wsl.localhost\Ubuntu\home\dchen327\coding\hmc\cv\pool-agent\test.py
+```
--- test.py
+++ test.py
@@ -2,15 +2,19 @@ import pygetwindow as pgw
 import mss
 import cv2
 import numpy as np
+import subprocess
+from time import sleep
 
 # Activate the window
 try:
     window = pgw.getWindowsWithTitle('8Ball')[0]
     window.activate()
-    window.maximize()
 except:
     print("Window not found or couldn't be activated")
     # run scrcpy --no-audio --window-title=8Ball
+    subprocess.run(['scrcpy', '--no-audio', '--fullscreen', '--window-title=8Ball'], check=True)
+    window = pgw.getWindowsWithTitle('8Ball')[0]
+    window.activate()
 
 # Get your actual monitor dimensions
 with mss.mss() as sct:
@@ -25,12 +29,15 @@ with mss.mss() as sct:
         "height": target_monitor["height"]
     }
     
-    # Capture the screen
-    sct_img = sct.grab(monitor)
-    
-    # Convert to CV2 format
-    img_cv2 = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGRA2BGR)
-    
-    # Write to file
-    cv2.imwrite('screenshot.png', img_cv2)
+    # every 3s for 10 times
+    for i in range(10):
+        # Capture the screen
+        sct_img = sct.grab(monitor)
+        
+        # Convert to CV2 format
+        img_cv2 = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGRA2BGR)
+        
+        # Write to file
+        cv2.imwrite(f'screenshot_{i}.png', img_cv2)
+        sleep(3)
 
