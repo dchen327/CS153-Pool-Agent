e065957e86617260eabbf3e4dc2eed0849509250
David Chen <davidc3287@gmail.com>
2025-04-03 20:22:01 -0700

    clean up

--- test.py
+++ test.py
@@ -15,60 +15,67 @@ root = Path(__file__).parent
 screenshot_dir = root / 'screenshots'
 screenshot_dir.mkdir(exist_ok=True)
 
-# Activate the window
-try:
-    window = pgw.getWindowsWithTitle('8Ball')[0]
-    window.activate()
-except:
-    print("Window not found or couldn't be activated")
-    # run scrcpy --no-audio --window-title=8Ball
-    process = subprocess.Popen(['scrcpy', '--no-audio', '--fullscreen', '--window-title=8Ball'])
-    sleep(4)  # Give scrcpy some time to open the window
-    window = pgw.getWindowsWithTitle('8Ball')[0]
-    window.activate()
+def setup():
+    # Activate the window
+    try:
+        window = pgw.getWindowsWithTitle('8Ball')[0]
+        window.activate()
+    except:
+        print("Window not found or couldn't be activated")
+        # run scrcpy --no-audio --window-title=8Ball
+        subprocess.Popen(['scrcpy', '--no-audio', '--fullscreen', '--window-title=8Ball'])
+        sleep(4)  # Give scrcpy some time to open the window
+        # wait until the window is available
+        for _ in range(10):  # Try up to 10 times
+            if pgw.getWindowsWithTitle('8Ball'):
+                window = pgw.getWindowsWithTitle('8Ball')[0]
+                window.activate()
+                break
+                sleep(1)
+        else:
+            print("Failed to find or activate the '8Ball' window after 10 attempts")
 
-# Get your actual monitor dimensions
-# with mss.mss() as sct:
-#     print("Available monitors: ", sct.monitors)
-#     target_monitor = sct.monitors[1]  # Primary monitor
-    
-#     # Create the capture area using actual monitor dimensions
-#     monitor = {
-#         "top": target_monitor["top"], 
-#         "left": target_monitor["left"], 
-#         "width": target_monitor["width"], 
-#         "height": target_monitor["height"]
-#     }
-    
-#     # every 3s for 10 times
-#     for i in range(10):
-#         # Capture the screen
-#         sct_img = sct.grab(monitor)
+def collect_data():
+    with mss.mss() as sct:
+        target_monitor = sct.monitors[1]  # Primary monitor
         
-#         # Convert to CV2 format
-#         img_cv2 = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGRA2BGR)
+        # create capture area using actual monitor dimensions
+        monitor = {
+            "top": target_monitor["top"], 
+            "left": target_monitor["left"], 
+            "width": target_monitor["width"], 
+            "height": target_monitor["height"]
+        }
         
-#         # Write to file, save in screenshots folder
-#         screenshot_path = screenshot_dir / f'screenshot_{i}.png'
-#         cv2.imwrite(str(screenshot_path), img_cv2)
-#         print(f"Screenshot {i} taken")
-#         sleep(3)
+        # every 3s for 10 times
+        for i in range(10):
+            sct_img = sct.grab(monitor)
+            img_cv2 = cv2.cvtColor(np.array(sct_img), cv2.COLOR_BGRA2BGR)
+            
+            # write to file, save in screenshots folder
+            screenshot_path = screenshot_dir / f'screenshot_{i}.png'
+            cv2.imwrite(str(screenshot_path), img_cv2)
+            print(f"Screenshot {i} taken")
+            sleep(3)
+
 def shoot_ball(coords, power=100):
     ''' Given (x, y) coordinates and power, click to aim and shoot the ball'''
-    # Click to aim
+    # click to aim
     pag.click(coords[0], coords[1])
     sleep(0.3)  # Wait for the aim to settle
-    # Drag power stick down to shoot
+    # drag power stick down to shoot
     pag.mouseDown(POWER_STICK_TOP[0], POWER_STICK_TOP[1])
-    # Calculate the drag distance based on power
+    # calculate the drag distance based on power
     drag_distance = int((POWER_STICK_BOTTOM[1] - POWER_STICK_TOP[1]) * (power / 100))
-    # Drag down to shoot
+    # drag down to shoot
     pag.moveTo(POWER_STICK_BOTTOM[0], POWER_STICK_TOP[1] + drag_distance, duration=0.25)
-    # Release the mouse button
     pag.mouseUp()
-    # Wait for the shot to complete
+    # wait for the shot to complete
     sleep(0.5)
 
+# TODO: figure out how to determine if it's my turn to shoot (maybe amt of green in time bar is changing?)
+# or just check the outline on the profile pic
+# or check for motion? if not motion in game area, maybe is my turn to shoot
 
 coords = (1500, 775)
 for _ in range(1):
